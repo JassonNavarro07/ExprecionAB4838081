5b426a101e48657409ba031bc12bb8dbb005b3a6
Jasson <Jasson@DESKTOP-S5KM2ME>
2024-06-02 23:41:16 -0500

    Agregar archivos de proyecto.

---
+++ ExprecionAB4838081/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExprecionAB4838081
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ExprecionAB4838081/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace ExprecionAB4838081
+{
+
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+           if (!string.IsNullOrEmpty(entry1.Text)&& !string.IsNullOrEmpty(entry2.Text)) 
+           {
+                double resultado, A, B;
+                A = Convert.ToDouble(entry1.Text);
+                B = Convert.ToDouble(entry1.Text);
+                resultado = Math.Pow((A + B),2)/2;
+                entryr.Text = resultado.ToString();
+            }
+           else
+            {
+                DisplayAlert("Error","Introduce todos los numeros", "ok");
+            }
+        }
+    }
+
+}
